3476b9535b51e6deb5e8743d340a95d58876f68f
Jason Lee <awesomelemonade@users.noreply.github.com>
2023-01-10 23:12:31 -0500

    Basic launcher micro (#5)
    * Launchers consider attacking after moving
    
    * Copy to oldLauncherMicro
    
    * Split into moveWithAction and moveWithoutAction
    
    * Adjust attack

--- src/oldLauncherMicro/robots/Launcher.java
+++ src/oldLauncherMicro/robots/Launcher.java
@@ -1,12 +1,12 @@
-package sprintBot.robots;
+package oldLauncherMicro.robots;
 
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
-import sprintBot.util.*;
+import oldLauncherMicro.util.*;
 
-import static sprintBot.util.Constants.rc;
+import static oldLauncherMicro.util.Constants.rc;
 
 public class Launcher implements RunnableBot {
     @Override
@@ -20,7 +20,7 @@ public class Launcher implements RunnableBot {
             EnemyHqGuesser.forEach(location -> Debug.setIndicatorDot(Profile.ATTACKING, location, 0, 0, 0)); // black
         }
         action();
-        move();
+        moveWithAction();
         action();
     }
 
@@ -28,65 +28,20 @@ public class Launcher implements RunnableBot {
         if (!rc.isActionReady()) {
             return;
         }
-        RobotInfo enemy = getBestImmediateAttackTarget();
+        RobotInfo enemy = Util.getClosestEnemyRobot(robot -> robot.type != RobotType.HEADQUARTERS);
         if (enemy != null) {
             MapLocation enemyLocation = enemy.location;
             tryAttack(enemyLocation);
         }
     }
 
-    public RobotInfo getBestImmediateAttackTarget() {
-        RobotInfo bestEnemy = null;
-        double bestScore = -Double.MAX_VALUE;
-        for (int i = Cache.ENEMY_ROBOTS.length; --i >= 0; ) {
-            RobotInfo enemy = Cache.ENEMY_ROBOTS[i];
-            MapLocation enemyLocation = enemy.location;
-            if (enemy.type != RobotType.HEADQUARTERS &&
-                    Cache.MY_LOCATION.isWithinDistanceSquared(enemyLocation, Constants.ROBOT_TYPE.actionRadiusSquared)) {
-                double score = getImmediateAttackScore(enemy);
-                if (score > bestScore) {
-                    bestScore = score;
-                    bestEnemy = enemy;
-                }
-            }
-        }
-        return bestEnemy;
-    }
-
-    public double getImmediateAttackScore(RobotInfo robot) {
-        // function of robotType, health, canKill, movementCooldown, actionCooldown, distance?
-        double score = 0;
-        switch (robot.type) {
-            case LAUNCHER:
-                score += 100000;
-                break;
-        }
-        if (robot.health <= Constants.ROBOT_TYPE.damage) {
-            score += 10000;
-        }
-        score -= robot.health * 100; // max health is 40
-        score -= robot.location.distanceSquaredTo(Cache.MY_LOCATION);
-        return score;
-    }
-
-    public void move() {
-        if (rc.isActionReady()) {
-            moveWithAction();
-        } else {
-            moveWithoutAction();
-        }
-    }
-
     public void moveWithoutAction() {
         if (!rc.isMovementReady()) {
-            return;
-        }
-        // TODO: consider moving to a better attacking square
-        // TODO: consider kiting
-        // Move towards our hq?
-        MapLocation location = Communication.getClosestAllyHQ();
-        if (location != null) {
-            Util.tryPathfindingMove(location);
+            // Move towards our hq?
+            MapLocation location = Communication.getClosestAllyHQ();
+            if (location != null) {
+                Util.tryPathfindingMove(location);
+            }
         }
     }
 
@@ -115,11 +70,7 @@ public class Launcher implements RunnableBot {
             // Attack
             MapLocation enemyLocation = enemy.location;
             // TODO: only move towards when there is an action
-            if (enemyLocation.isWithinDistanceSquared(Cache.MY_LOCATION, Constants.ROBOT_TYPE.actionRadiusSquared)) {
-                // TODO: consider kiting?
-            } else {
-                Util.tryPathfindingMove(enemyLocation);
-            }
+            Util.tryPathfindingMove(enemyLocation);
         }
     }
 
