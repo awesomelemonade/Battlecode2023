7f6e4ef976d48f5549f90b3eaf7eada324672512
awesomelemonade <awesomelemonade@gmail.com>
2023-01-11 12:44:31 -0500

    Add beforeBlacklist

---
+++ src/beforeBlacklist/util/Profile.java
@@ -0,0 +1,20 @@
+package beforeBlacklist.util;
+
+public enum Profile {
+    ERROR_STATE(true),
+    PATHFINDING(false),
+    EXPLORER(false),
+    MINING(true),
+    ATTACKING(true),
+    REPEAT_MOVE(true);
+
+    private final boolean enabled;
+
+    private Profile(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean enabled() {
+        return Constants.DEBUG_PROFILES && enabled;
+    }
+}
---
+++ src/beforeBlacklist/util/RunnableBot.java
@@ -0,0 +1,8 @@
+package beforeBlacklist.util;
+
+import battlecode.common.GameActionException;
+
+public interface RunnableBot {
+    public void init() throws GameActionException;
+    public void loop() throws GameActionException;
+}
