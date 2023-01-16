33fa2c4bdf0ab03b9b91e15ca21d94a09d2a64c4
Jason Lee <awesomelemonade@gmail.com>
2023-01-16 14:22:05 -0500

    Fix tryBuildLauncher()

--- src/sprintBot/robots/Headquarters.java
+++ src/sprintBot/robots/Headquarters.java
@@ -293,10 +293,12 @@ public class Headquarters implements RunnableBot {
     }
 
     public static boolean tryBuildLauncher() {
-        // if 2 or more enemy attackers within radius 16 OR 5 or more enemy attackers within vision radius
-        if (Util.numEnemyAttackersWithin(Cache.MY_LOCATION, 16) >= 2
-                || Util.numEnemyAttackersWithin(Cache.MY_LOCATION, Constants.ROBOT_TYPE.visionRadiusSquared) >= 5) {
-            return false;
+        if (Cache.ALLY_ROBOTS.length == 0) {
+            // if 2 or more enemy attackers within radius 16 OR 5 or more enemy attackers within vision radius
+            if (Util.numEnemyAttackersWithin(Cache.MY_LOCATION, 16) >= 2
+                    || Util.numEnemyAttackersWithin(Cache.MY_LOCATION, Constants.ROBOT_TYPE.visionRadiusSquared) >= 5) {
+                return false;
+            }
         }
         MapLocation macroLocation = getMacroAttackLocation();
         Debug.setIndicatorLine(Profile.ATTACKING, Cache.MY_LOCATION, macroLocation, 255, 128, 0);
