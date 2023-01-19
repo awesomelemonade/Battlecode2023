60526a6801b596daa29650c0ca0e54a9128f3b50
ldcWV <ldorianchen@gmail.com>
2023-01-19 15:55:13 -0500

    sequential version works

---
+++ rust/microtrainer/src/game.rs
@@ -0,0 +1,299 @@
+use std::collections::HashMap;
+
+#[derive(PartialEq, Debug, Copy, Clone)]
+pub enum Team {
+    Red, Blue
+}
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum Direction {
+    Center, North, Northeast, East, Southeast, South, Southwest, West, Northwest
+}
+
+impl Direction {
+    pub fn dx(&self) -> i32 {
+        match *self {
+            Self::Center => 0,
+            Self::North => 0,
+            Self::Northeast => 1,
+            Self::East => 1,
+            Self::Southeast => 1,
+            Self::South => 0,
+            Self::Southwest => -1,
+            Self::West => -1,
+            Self::Northwest => -1,
+        }
+    }
+    
+    pub fn dy(&self) -> i32 {
+        match *self {
+            Self::Center => 0,
+            Self::North => 1,
+            Self::Northeast => 1,
+            Self::East => 0,
+            Self::Southeast => -1,
+            Self::South => -1,
+            Self::Southwest => -1,
+            Self::West => 0,
+            Self::Northwest => 1,
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Position {
+    pub fn add(&self, dir: Direction) -> Self {
+        Position {
+            x: self.x + dir.dx(),
+            y: self.y + dir.dy(),
+        }
+    }
+
+    pub fn distance_squared(&self, other: Position) -> i32 {
+        (self.x - other.x).pow(2) + (self.y - other.y).pow(2)
+    }
+}
+
+pub struct GameManager<F1, F2>
+where F1: Fn(&mut GameState, u32) -> (),
+      F2: Fn(&mut GameState, u32) -> () {
+    pub state: GameState,
+    red_controller: F1,
+    blue_controller: F2,
+}
+
+impl <F1, F2> GameManager<F1, F2>
+where F1: Fn(&mut GameState, u32) -> (),
+      F2: Fn(&mut GameState, u32) -> () {
+    pub fn new(state: GameState, red_controller: F1, blue_controller: F2) -> Self {
+        GameManager {
+            state,
+            red_controller,
+            blue_controller,
+        }
+    }
+
+    pub fn step_game(&mut self) {
+        if self.state.is_game_over() {
+            panic!("Tried stepping game when game is over");
+        }
+
+        self.state.subturn_count += 1;
+        if self.state.turn_queue_index == self.state.turn_queue.len() {
+            self.state.turn_queue_index = 0;
+            self.state.turn_count += 1;
+            for (_, robot) in &mut self.state.robots {
+                robot.action_cooldown = robot.action_cooldown.saturating_sub(10);
+                robot.move_cooldown = robot.move_cooldown.saturating_sub(10);
+            }
+            self.state.attack_vectors.clear();
+        }
+
+        let id = self.state.turn_queue[self.state.turn_queue_index];
+        let team = &self.state.robots.get(&id).expect("Invalid id").team;
+        if *team == Team::Red {
+            (self.red_controller)(&mut self.state, id);
+        } else {
+            (self.blue_controller)(&mut self.state, id);
+        }
+
+        self.state.turn_queue_index += 1;
+    }
+}
+
+#[derive(Clone)]
+pub struct RobotInfo {
+    pub id: u32,
+    pub health: u32,
+    pub action_cooldown: u32,
+    pub move_cooldown: u32,
+    pub team: Team,
+    pub pos: Position,
+}
+
+#[derive(Clone)]
+pub struct GameState {
+    pub width: i32,
+    pub height: i32,
+    next_robot_id: u32,
+    pub turn_queue: Vec<u32>,
+    pub turn_queue_index: usize,
+    pub turn_count: u32,
+    pub subturn_count: u32,
+    pub robots: HashMap<u32, RobotInfo>,
+    pub map: [[i32; 64]; 64],
+    pub attack_vectors: Vec<(Team, Position, Position)>,
+}
+
+impl GameState {
+    pub fn new(width: i32, height: i32) -> Self {
+        GameState {
+            width,
+            height,
+            next_robot_id: 0,
+            turn_queue: Vec::new(),
+            turn_queue_index: 0,
+            turn_count: 0,
+            subturn_count: 0,
+            robots: HashMap::new(),
+            map: [[-1; 64]; 64],
+            attack_vectors: Vec::new(),
+        }
+    }
+    
+    pub fn is_game_over(&self) -> bool {
+        let mut has_red = false;
+        let mut has_blue = false;
+        for (_, robot) in &(self.robots) {
+            if robot.team == Team::Red {
+                has_red = true;
+            } else {
+                has_blue = true;
+            }
+        }
+        !has_red || !has_blue
+    }
+
+    pub fn get_outcome(&self) -> Team {
+        assert!(self.is_game_over());
+        for (_, robot) in &(self.robots) {
+            return robot.team;
+        }
+        panic!("wtf");
+    }
+
+    pub fn in_bounds(&self, pos: Position) -> bool {
+        0 <= pos.x && pos.x < self.width && 0 <= pos.y && pos.y < self.height
+    }
+
+    pub fn place_robot(&mut self, team: Team, pos: Position) {
+        if !self.in_bounds(pos) {
+            panic!("Cannot place robot out of bounds");
+        }
+
+        let id = self.next_robot_id;
+        self.next_robot_id += 1;
+
+        let robot = RobotInfo {
+            id,
+            health: 150,
+            action_cooldown: 0,
+            move_cooldown: 0,
+            team,
+            pos,
+        };
+
+        self.turn_queue.push(id);
+        self.robots.insert(id, robot);
+        self.map[pos.x as usize][pos.y as usize] = id as i32;
+    }
+
+    pub fn can_move(&self, id: u32, direction: Direction) -> bool {
+        let robot = self.robots.get(&id).expect("Invalid id");
+
+        if direction == Direction::Center {
+            return false;
+        }
+
+        if robot.move_cooldown >= 10 {
+            return false;
+        }
+
+        let new_pos = robot.pos.add(direction);
+        if !self.in_bounds(new_pos) {
+            return false;
+        }
+        if self.map[new_pos.x as usize][new_pos.y as usize] != -1 {
+            return false;
+        }
+
+        true
+    }
+
+    pub fn do_move(&mut self, id: u32, direction: Direction) {
+        if !self.can_move(id, direction) {
+            panic!("Cannot move");
+        }
+        
+        let robot = self.robots.get_mut(&id).expect("Invalid id");
+        let new_pos = robot.pos.add(direction);
+
+        robot.move_cooldown += 15;
+        self.map[robot.pos.x as usize][robot.pos.y as usize] = -1;
+        robot.pos = new_pos;
+        self.map[new_pos.x as usize][new_pos.y as usize] = robot.id as i32;
+    }
+
+    pub fn can_attack(&self, id: u32, target_id: u32) -> bool {
+        let robot = self.robots.get(&id).expect("Invalid id");
+        let enemy = self.robots.get(&target_id).expect("Invalid id");
+
+        if robot.team == enemy.team {
+            return false;
+        }
+
+        if robot.action_cooldown >= 10 {
+            return false;
+        }
+
+        robot.pos.distance_squared(enemy.pos) <= 16
+    }
+
+    pub fn do_attack(&mut self, id: u32, target_id: u32) {
+        if !self.can_attack(id, target_id) {
+            panic!("Cannot attack");
+        }
+        
+        let mut robot = self.robots.get_mut(&id).expect("Invalid id");
+        robot.action_cooldown += 10;
+        let robot_pos = robot.pos;
+        let robot_team = robot.team;
+        let mut enemy = self.robots.get_mut(&target_id).expect("Invalid id");
+        self.attack_vectors.push((robot_team, robot_pos, enemy.pos));
+        
+        enemy.health = enemy.health.saturating_sub(30);
+        if enemy.health == 0 {
+            let idx = self.turn_queue.iter().position(|x| *x == target_id).unwrap();
+            self.turn_queue.remove(idx);
+            if idx < self.turn_queue_index {
+                self.turn_queue_index -= 1;
+            }
+            self.map[enemy.pos.x as usize][enemy.pos.y as usize] = -1;
+            self.robots.remove(&target_id);
+        }
+    }
+
+    pub fn sense_nearby_robots(&self, id: u32) -> Vec<&RobotInfo> {
+        let robot = self.robots.get(&id).expect("Invalid id");
+        let mut res = Vec::new();
+        for (_, other) in &self.robots {
+            if id != other.id && robot.pos.distance_squared(other.pos) <= 20 {
+                res.push(other);
+            }
+        }
+        res
+    }
+
+    pub fn get_nearest_enemy_omnipotent(&self, id: u32) -> Option<&RobotInfo> {
+        let robot = self.robots.get(&id).expect("Invalid id");
+
+        let mut best_enemy: Option<&RobotInfo> = None;
+        let mut best_dist = 1000000;
+        for (_, other) in &self.robots {
+            if robot.team != other.team {
+                let dist = robot.pos.distance_squared(other.pos);
+                if dist < best_dist {
+                    best_dist = dist;
+                    best_enemy = Some(other);
+                }
+            }
+        }
+
+        best_enemy
+    }
+}
---
+++ rust/microtrainer/src/micro.rs
@@ -0,0 +1,224 @@
+use rand::seq::SliceRandom;
+
+use crate::game::GameState;
+use crate::game::Direction;
+use crate::game::Position;
+use crate::game::RobotInfo;
+
+const ALL_DIRS: [Direction; 8] = [
+    Direction::North,
+    Direction::Northeast,
+    Direction::East,
+    Direction::Southeast,
+    Direction::South,
+    Direction::Southwest,
+    Direction::West,
+    Direction::Northwest,
+];
+
+pub fn random_micro(state: &mut GameState, id: u32) {
+    let mut rng = rand::thread_rng();
+    let dir = *ALL_DIRS.choose(&mut rng).unwrap();
+
+    if state.can_move(id, dir) {
+        state.do_move(id, dir);
+    }
+}
+
+pub fn get_scored_micro(coeffs: [f32; 12]) -> impl Fn(&mut GameState, u32) -> () {
+    move |state, id| {
+        let robot = state.robots.get(&id).unwrap();
+
+        let sensed_robots = state.sense_nearby_robots(id);
+        let mut enemy_robots = Vec::new();
+        let mut ally_robots = Vec::new();
+        for other in sensed_robots {
+            if robot.team == other.team {
+                ally_robots.push(other);
+            } else {
+                enemy_robots.push(other);
+            }
+        }
+
+        let get_score = |pos: Position| -> f32 {
+            let mut dist_to_nearest_enemy = 1000;
+            let mut lowest_enemy = None;
+            let mut lowest_enemy_health = 200;
+            let mut num_enemies = 0;
+            for robot in &enemy_robots {
+                let dist = robot.pos.distance_squared(pos);
+
+                dist_to_nearest_enemy = std::cmp::min(dist_to_nearest_enemy, dist);
+                if dist <= 16 {
+                    lowest_enemy_health = std::cmp::min(lowest_enemy_health, robot.health);
+                    lowest_enemy = Some(robot);
+                    num_enemies += 1;
+                }
+            }
+            let enemy_close = if dist_to_nearest_enemy <= 9 {1.0} else {0.0};
+            let enemy_middle = if 9 < dist_to_nearest_enemy && dist_to_nearest_enemy <= 16 {1.0} else {0.0};
+            let enemy_far = if 16 < dist_to_nearest_enemy {1.0} else {0.0};
+
+            let mut num_allies_close = 0;
+            let mut num_allies_far = 0;
+            if lowest_enemy.is_some() {
+                for robot in &ally_robots {
+                    if robot.action_cooldown >= 10 {
+                        continue;
+                    }
+                    let dist = robot.pos.distance_squared(lowest_enemy.unwrap().pos);
+
+                    if dist <= 16 {
+                        num_allies_close += 1;
+                    } else if dist <= 26 && robot.move_cooldown < 10 {
+                        num_allies_far += 1;
+                    }
+                }
+            }
+
+            let ready_to_attack = if robot.action_cooldown < 10 {1.0} else {0.0};
+            let new_movement_cooldown = if robot.pos == pos {robot.move_cooldown} else {robot.move_cooldown + 10};
+
+            coeffs[0] * enemy_close * ready_to_attack +
+            coeffs[1] * enemy_middle * ready_to_attack +
+            coeffs[2] * enemy_far * ready_to_attack +
+            coeffs[3] * enemy_close * (1.0 - ready_to_attack) +
+            coeffs[4] * enemy_middle * (1.0 - ready_to_attack) +
+            coeffs[5] * enemy_far * (1.0 - ready_to_attack) +
+            coeffs[6] * lowest_enemy_health as f32 / 200.0 +
+            coeffs[7] * num_enemies as f32 +
+            coeffs[8] * num_allies_close as f32 / 5.0 +
+            coeffs[9] * num_allies_far as f32 / 5.0 +
+            coeffs[10] * ready_to_attack +
+            coeffs[11] * new_movement_cooldown as f32 / 10.0
+        };
+
+        let mut best_dir = Direction::Center;
+        let mut best_score = get_score(robot.pos.add(Direction::Center));
+        for dir in ALL_DIRS {
+            if !state.can_move(id, dir) {
+                continue;
+            }
+            let score = get_score(robot.pos.add(dir));
+            if score > best_score {
+                best_score = score;
+                best_dir = dir;
+            }
+        }
+        if state.can_move(id, best_dir) {
+            state.do_move(id, best_dir);
+        }
+    }
+}
+
+pub fn sprint_micro(state: &mut GameState, id: u32) {
+    fn get_best_move_direction<F>(state: &GameState, id: u32, scorer: F) -> Direction
+    where F: Fn(&GameState, u32, Position) -> f32 {
+        let robot = state.robots.get(&id).unwrap();
+        let mut best_direction = Direction::Center;
+        let mut best_score = -1e18;
+        
+        for direction in ALL_DIRS {
+            if !state.can_move(robot.id, direction) {
+                continue;
+            }
+            let position = robot.pos.add(direction);
+            let score = scorer(state, id, position);
+            if score > best_score {
+                best_score = score;
+                best_direction = direction;
+            }
+        }
+        best_direction
+    }
+
+    fn get_score_for_kiting(state: &GameState, id: u32, pos: Position) -> f32 {
+        let robot = state.robots.get(&id).unwrap();
+        let sensed_robots = state.sense_nearby_robots(id);
+
+        let mut score = 0.0;
+        
+        // prefer squares where attackers can't see you
+        let mut num_enemies = 0;
+        for other in sensed_robots {
+            if robot.team != other.team && pos.distance_squared(other.pos) <= 20 {
+                num_enemies += 1;
+            }
+        }
+        score -= num_enemies as f32 * 100000.0;
+
+        // prefer squares where you're further away from the closest enemy
+        let closest_enemy = state.get_nearest_enemy_omnipotent(id).unwrap();
+        let dist = robot.pos.distance_squared(closest_enemy.pos);
+        assert!(dist <= 20);
+        score += dist as f32 * 1000.0;
+
+        // prefer diagonals over straight directions
+        if robot.pos.distance_squared(pos) == 2 {
+            score += 50.0;
+        }
+
+        score
+    }
+
+    fn get_score_with_action_single_enemy_attacker(state: &GameState, id: u32, pos: Position, enemy: &RobotInfo) -> f32 {
+        let robot = state.robots.get(&id).unwrap();
+
+        let mut score = 0.0;
+
+        // todo: prefer non-clouds if we're not in a cloud
+
+        // prefer squares that we can attack the enemy
+        if pos.distance_squared(enemy.pos) <= 16 {
+            score += 1000000.0;
+        }
+
+        // prefer straight moves
+        if robot.pos.distance_squared(pos) == 1 {
+            score += 1000.0;
+        }
+
+        // prefer squares where you're further away from the enemy
+        score += pos.distance_squared(enemy.pos) as f32;
+
+        score
+    }
+
+    let robot = state.robots.get(&id).unwrap();
+    let sensed_robots = state.sense_nearby_robots(id);
+    if robot.action_cooldown < 10 {
+        // getSingleAttackerOrNull
+        let mut enemy = None;
+        for other in sensed_robots {
+            if robot.team != other.team {
+                if enemy.is_some() {
+                    enemy = None;
+                    break;
+                }
+                enemy = Some(other);
+            }
+        }
+
+        if let Some(enemy) = enemy {
+            // shouldAttackSingleEnemyWithAction
+            let damage = 30;
+            let num_attacks_to_enemy = (enemy.health + damage - 1) / damage;
+            let num_attacks_to_us = (robot.health + damage - 1) / damage;
+            let should_attack = num_attacks_to_us >= num_attacks_to_enemy;
+
+            if should_attack {
+                let dir = get_best_move_direction(state, id, |state, id, pos| {
+                    get_score_with_action_single_enemy_attacker(state, id, pos, enemy)
+                });
+                if state.can_move(id, dir) {
+                    state.do_move(id, dir);
+                }
+                return;
+            }
+        }
+    }
+    let dir = get_best_move_direction(state, id, get_score_for_kiting);
+    if state.can_move(id, dir) {
+        state.do_move(id, dir);
+    }
+}
