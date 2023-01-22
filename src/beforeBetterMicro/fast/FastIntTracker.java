9ed4004bc6e67190e36ab547c3a4cf4bd20e7ef6
Jason Lee <awesomelemonade@gmail.com>
2023-01-22 18:38:46 -0500

    Add beforeBetterMicro

---
+++ src/beforeBetterMicro/fast/FastIntTracker.java
@@ -0,0 +1,33 @@
+package beforeBetterMicro.fast;
+
+public class FastIntTracker {
+    private int[] data;
+    private int index;
+    private int sum;
+    private int size;
+
+    public FastIntTracker(int size) {
+        this.data = new int[size];
+        this.index = 0;
+        this.sum = 0;
+        this.size = size;
+    }
+
+    public void add(int data) {
+        sum = sum + data - this.data[index];
+        this.data[index] = data;
+        index = (index + 1) % size;
+    }
+
+    public void incrementLast() {
+        sum++;
+        this.data[(index + (size - 1)) % size]++;
+    }
+
+    public int sum() {
+        return sum;
+    }
+    public double average() {
+        return ((double) sum) / ((double) size);
+    }
+}
---
+++ src/beforeBetterMicro/pathfinder/BFSCheckpoints.java
@@ -0,0 +1,12 @@
+package beforeBetterMicro.pathfinder;
+
+import battlecode.common.Direction;
+
+public class BFSCheckpoints {
+//    private static final int SIZE = Checkpoints.SIZE;
+//    private int[][] distances = new int[SIZE][SIZE];
+//    private Direction[][] prev = new Direction[SIZE][SIZE];
+    public static void bfs() {
+        // bfs from current location
+    }
+}
